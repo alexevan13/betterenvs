ede6f73a46e3f08a01d948711c78b9273f97798b
Alex Christian <alex.christian@watchguard.com>
2024-09-28 17:34:48 -0400

    Add Better Envs Library

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     url='https://github.com/PhantomWall/phantom-tools.git',
     author="Alex Christian",
     author_email='13alexchristian@example.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=REQUIREMENTS,
